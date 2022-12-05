d7af1bd1960e8b47c3eba0f860eb2e9d0ff2d7e9
Gabriel <gamcode98@gmail.com>
2022-12-05 15:53:39 -0300

    a simple refactor

--- public/js/app.js
+++ public/js/app.js
@@ -1,46 +1,44 @@
-const d = document;
-const $main = d.getElementById("advice-slip-api");
-const $fragment = d.createDocumentFragment();
-let $section = d.createElement("section");
-let $h2 = d.createElement("h2");
-let $blockquote = d.createElement("blockquote");
-let $div = d.createElement("div");
-$div.classList.add("pattern-divider");
-let $btn = d.createElement("button");
-$btn.setAttribute("id", "generate-advice");
-let $img = d.createElement("img");
-$img.setAttribute("src", "./assets/images/icon-dice.svg");
-let $imgLoader = d.createElement("img");
-$imgLoader.classList.add("loader");
-$imgLoader.setAttribute("src", "./assets/images/bars.svg");
+const $main = document.getElementById("main")
+let $section = document.createElement("section")
+let $h2 = document.createElement("h2")
+let $blockquote = document.createElement("blockquote")
+let $div = document.createElement("div")
+$div.classList.add("pattern-divider")
+let $btn = document.createElement("button")
+$btn.setAttribute("id", "generate-advice")
+let $img = document.createElement("img")
+$img.setAttribute("src", "./assets/images/icon-dice.svg")
+let $imgLoader = document.createElement("img")
+$imgLoader.classList.add("loader")
+$imgLoader.setAttribute("src", "./assets/images/bars.svg")
 
-$section.appendChild($h2);
-$section.appendChild($blockquote);
-$section.appendChild($div);
-$btn.appendChild($img);
-$section.appendChild($btn);
+$section.appendChild($h2)
+$section.appendChild($blockquote)
+$section.appendChild($div)
+$btn.appendChild($img)
+$section.appendChild($btn)
 
-async function getData() {
+const getData = async () => {
   try {
-    $h2.textContent = "Advice loading ...";
-    $blockquote.textContent = "";
-    $main.appendChild($imgLoader);
-    let res = await fetch("https://api.adviceslip.com/advice");
-    let json = await res.json();
-    if (!res.ok) throw { status: res.status, statusText: res.statusText };
+    $h2.textContent = "Advice loading..."
+    $btn.replaceChild($imgLoader, $img)
+    let response = await fetch("https://api.adviceslip.com/advice")
+    let json = await response.json()
+    if (!response.ok) throw { status: response.status, message: response.statusText }
+    $btn.replaceChild($img, $imgLoader)
+    $main?.appendChild($section)
+    $h2.textContent = `Advice #${json.slip.id}`
+    $blockquote.textContent = `${json.slip.advice}`
+  } catch (error) {
+    let message = error.message || "Something went wrong"
+    $blockquote.textContent = `Error: ${error.status}: ${message}`
 
-    $h2.textContent = `Advice #${json.slip.id}`;
-    $blockquote.textContent = `${json.slip.advice}`;
-  } catch (err) {
-    let message = err.statusText || "Somenthing was wrong";
-    $blockquote.textContent = `Error:  ${err.status}: ${message}`;
   }
-  $main.removeChild($imgLoader);
-  $main.appendChild($section);
 }
-getData();
 
 $btn.addEventListener("click", (e) => {
-  e.preventDefault();
-  getData();
-});
+  e.preventDefault()
+  getData()
+})
+
+document.addEventListener("DOMContentLoaded", getData)
